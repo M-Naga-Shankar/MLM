1cd5afad0c9dd934bbc98f23e4dfe29bf92480c1
nagashankar-01 <138195625+nagashankar-01@users.noreply.github.com>
2023-12-29 20:45:08 +0530

    Update config.php

--- mlm/admin/includes/config.php
+++ mlm/admin/includes/config.php
@@ -1,5 +1,5 @@
 <?php
 error_reporting(0);
-$conn=mysqli_connect("localhost","u405813026_sriadityadevel","Cq92QNaj:q=","u405813026_sriadityadevel");
+$conn=mysqli_connect("localhost","root","password","db_name");
 
 ?>
