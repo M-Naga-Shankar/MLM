4daf2c8196b49e1194d4ab45be24064812863d1e
nagashankar-01 <138195625+nagashankar-01@users.noreply.github.com>
2023-12-29 20:46:02 +0530

    Update config.php

--- mlm/user/includes/config.php
+++ mlm/user/includes/config.php
@@ -1,5 +1,5 @@
 <?php
 error_reporting(0);
-$conn=mysqli_connect("localhost","u925430035_adityadevelope","5#HilMSBd","u925430035_adityadevelope");
+$conn=mysqli_connect("localhost","user","password","db_name");
 
 ?>
